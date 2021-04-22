904860439cd778e3ae6acd2637b49b76b53beee1
Rayven <rayven.burns@gmail.com>
2021-04-21 20:51:19 -0400

    trying to print the square

--- magicsq.py
+++ magicsq.py
@@ -1,5 +1,5 @@
 import math
-
+import random
 
 
 # This isn't apart of algorithm, only for nice printing of square
@@ -52,3 +52,74 @@ def rule3(x,n,upleft):
     return((x + ((-1)**upleft * (-n+1)))%n**2)
 
 
+# Filling out the rest of the square
+    # One way to fill in the rest of the square is to "walk" randomly through it
+
+# First - determine the indices of our central entry
+center_i = math.floor(n/2)
+center_j = math.floor(n/2)
+
+# Then - randomly select a direction to "walk"
+entry_i = center_i
+entry_j = center_j
+
+where_we_can_go = ['up_left','up_right','down_left','down_right']
+
+
+def fillsquare(square,entry_i,entry_j,howfull):
+
+    while(sum(math.isnan(i) for row in square for i in row) > howfull):
+
+        where_we_can_go = []
+
+        if(entry_i < (n-1) and entry_j < (n-1)):
+            where_we_can_go.append('down_right')
+
+        if(entry_i < (n-1) and entry_j > 0):
+            where_we_can_go.append('down_left')
+
+        if(entry_i > 0 and entry_j < (n-1)):
+            where_we_can_go.append('up_right')
+        if(entry_i > 0 and entry_j > 0):
+            where_we_can_go.append('up_left')
+
+
+        where_to_go = random.choice(where_we_can_go)
+
+        if(where_to_go == 'up_right'):
+            new_entry_i = entry_i - 1
+            new_entry_j = entry_j + 1
+            square[new_entry_i][new_entry_j] = rule1(square[entry_i][entry_j],n,True)
+        if(where_to_go == 'down_left'):
+            new_entry_i = entry_i + 1
+            new_entry_j = entry_j - 1
+            square[new_entry_i][new_entry_j] = rule1(square[entry_i][entry_j],n,False)
+        if(where_to_go == 'up_left' and (entry_i + entry_j) != (n)):
+            new_entry_i = entry_i - 1
+            new_entry_j = entry_j - 1
+            square[new_entry_i][new_entry_j] = rule2(square[entry_i][entry_j],n,True)
+        if(where_to_go == 'down_right' and (entry_i + entry_j) != (n-2)):
+            new_entry_i = entry_i + 1
+            new_entry_j = entry_j + 1
+            square[new_entry_i][new_entry_j] = rule2(square[entry_i][entry_j],n,False)
+
+        # How to know if in entry that's near an antidiagonal - the entry just above the antidiagonal will have indices that sum to n-2, and entries just below antidiagonal will have indices that sum to n
+        if(where_to_go == 'up_left' and (entry_i + entry_j) == (n)):
+            new_entry_i = entry_i - 1
+            new_entry_j = entry_j - 1
+            square[new_entry_i][new_entry_j] = rule3(square[entry_i][entry_j],n,True)
+        if(where_to_go == 'down_right' and (entry_i + entry_j) == (n-2)):
+            new_entry_i = entry_i + 1
+            new_entry_j = entry_j + 1
+            square[new_entry_i][new_entry_j] = rule3(square[entry_i][entry_j],n,False)
+
+        entry_i = new_entry_i
+        entry_j = new_entry_j
+    
+    return(square)
+
+entry_i = math.floor(n/2) +1
+entry_j = math.floor(n/2)
+
+square = fillsquare(square, entry_i, entry_j,0)
+printsquare(square)
